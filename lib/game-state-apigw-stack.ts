2f0d86ac76b8f90c47b67214af359aecc60bf803
Bryce Colson <brycec17@vt.edu>
2023-02-01 10:54:39 -0500

    Added linters

--- lib/game-state-apigw-stack.ts
+++ lib/game-state-apigw-stack.ts
@@ -1,16 +1,9 @@
-import {
-  Stack,
-  StackProps
-} from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { WebSocketApi } from '@aws-cdk/aws-apigatewayv2-alpha';
 import { WebSocketLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
-import {
-  Code,
-  Function,
-  Runtime
-} from "aws-cdk-lib/aws-lambda";
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-import { S3Stack } from "./s3-stack";
+import { S3Stack } from './s3-stack';
 
 export interface GameStateApiGatewayStackProps extends StackProps {
   s3Stack: S3Stack;
