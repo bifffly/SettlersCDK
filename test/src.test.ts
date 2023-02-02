038b72951e35c3ad58d418b5f9ef750c85dd3725
Bryce Colson <brycec17@vt.edu>
2023-02-02 00:09:48 -0500

    Removed DynamoDB stack (#14)

--- test/src.test.ts
+++ test/src.test.ts
@@ -1,47 +1,43 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { DynamodbStack } from '../lib/dynamodb-stack';
-import { GameStateApiGatewayStack } from '../lib/game-state-apigw-stack';
-
-test('DynamoDB stack created', () => {
-  const app = new App();
-  const dynamoDbStack = new DynamodbStack(app, 'DynamoDbStack');
-  const dynamoDbTemplate = Template.fromStack(dynamoDbStack);
-  dynamoDbTemplate.resourceCountIs('AWS::DynamoDB::Table', 1);
-});
+import { GameStateStack } from '../lib/game-state-stack';
 
 describe('Game State API Gateway Stack', () => {
-  let gameStateApiTemplate: Template;
+  let gameStateTemplate: Template;
 
   beforeAll(() => {
     const app = new App();
 
-    const gameStateApiStack = new GameStateApiGatewayStack(app, 'GameStateApiGatewayStack');
-    gameStateApiTemplate = Template.fromStack(gameStateApiStack);
+    const gameStateStack = new GameStateStack(app, 'GameStateStack');
+    gameStateTemplate = Template.fromStack(gameStateStack);
+  });
+
+  test('creates DynamoDB resources', () => {
+    gameStateTemplate.resourceCountIs('AWS::DynamoDB::Table', 1);
   });
 
   test('creates Lambda resources', () => {
-    gameStateApiTemplate.hasResourceProperties('AWS::Lambda::Function', {
+    gameStateTemplate.hasResourceProperties('AWS::Lambda::Function', {
       Handler: 'game-state-handler.handler',
       Runtime: 'nodejs18.x',
     });
   });
 
   test('creates API Gateway resources', () => {
-    gameStateApiTemplate.resourceCountIs('AWS::ApiGatewayV2::Api', 1);
-    gameStateApiTemplate.hasResourceProperties('AWS::ApiGatewayV2::Api', {
+    gameStateTemplate.resourceCountIs('AWS::ApiGatewayV2::Api', 1);
+    gameStateTemplate.hasResourceProperties('AWS::ApiGatewayV2::Api', {
       Name: 'game-state-api',
       ProtocolType: 'WEBSOCKET',
       RouteSelectionExpression: '$request.body.action',
     });
-    gameStateApiTemplate.resourceCountIs('AWS::ApiGatewayV2::Route', 1);
-    gameStateApiTemplate.hasResourceProperties('AWS::ApiGatewayV2::Route', {
+    gameStateTemplate.resourceCountIs('AWS::ApiGatewayV2::Route', 1);
+    gameStateTemplate.hasResourceProperties('AWS::ApiGatewayV2::Route', {
       RouteKey: 'gamestate',
       RequestModels: {
         gamestate: 'gamestate-request-model',
       },
     });
-    gameStateApiTemplate.resourceCountIs('AWS::ApiGatewayV2::Model', 1);
-    gameStateApiTemplate.resourceCountIs('AWS::ApiGatewayV2::Integration', 1);
+    gameStateTemplate.resourceCountIs('AWS::ApiGatewayV2::Model', 1);
+    gameStateTemplate.resourceCountIs('AWS::ApiGatewayV2::Integration', 1);
   });
 });
