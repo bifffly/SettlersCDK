49fedaeceeb9cb586051a4f47b7527dc6f5a45f4
Bryce Colson <brycec17@vt.edu>
2023-02-02 00:25:14 -0500

    Refactored API routes

--- lib/game-state-stack.ts
+++ lib/game-state-stack.ts
@@ -1,17 +1,16 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { WebSocketApi } from '@aws-cdk/aws-apigatewayv2-alpha';
-import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { WebSocketApi, WebSocketStage } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-import { join } from 'path';
-import { CfnIntegration, CfnModel, CfnRoute } from 'aws-cdk-lib/aws-apigatewayv2';
-import { JsonSchemaType, JsonSchemaVersion } from 'aws-cdk-lib/aws-apigateway';
+import { WebSocketLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class GameStateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Table(this, 'game-state-table', {
+    const table = new Table(this, 'game-state-table', {
       partitionKey: {
         name: 'game-state-id',
         type: AttributeType.STRING,
@@ -21,43 +20,35 @@ export class GameStateStack extends Stack {
       writeCapacity: 1,
     });
 
-    const lambda = new Function(this, 'game-state-function', {
+    const connectLambda = new NodejsFunction(this, 'connect-function', {
+      entry: 'src/connect-handler.ts',
       runtime: Runtime.NODEJS_18_X,
-      handler: 'game-state-handler.handler',
-      code: Code.fromAsset(join(__dirname, '../src')),
-    });
-
-    const apiGateway = new WebSocketApi(this, 'game-state-api');
-
-    const schema = {
-      schema: JsonSchemaVersion.DRAFT4,
-      title: 'gamestate',
-      type: JsonSchemaType.OBJECT,
-      properties: {
-        gameId: { type: JsonSchemaType.STRING },
+      environment: {
+        TABLE_NAME: table.tableName,
       },
-    };
-
-    this.addRoute(apiGateway, 'gamestate', schema);
-
-    new CfnIntegration(this, 'gamestate-api-integration', {
-      apiId: apiGateway.apiId,
-      integrationType: 'AWS',
-      integrationUri: `arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${lambda.functionArn}/invocations`,
-    });
-  }
-
-  private addRoute(api: WebSocketApi, name: string, schema: any) {
-    const model = new CfnModel(this, `${name}-request-model`, {
-      apiId: api.apiId,
-      name: `${name}-request-model`,
-      schema,
     });
 
-    new CfnRoute(this, `${name}-route`, {
-      apiId: api.apiId,
-      routeKey: name,
-      requestModels: { [name]: model.name },
+    const disconnectLambda = new NodejsFunction(this, 'disconnect-function', {
+      entry: 'src/disconnect-handler.ts',
+      runtime: Runtime.NODEJS_18_X,
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    const apiGateway = new WebSocketApi(this, 'game-state-api', {
+      connectRouteOptions: {
+        integration: new WebSocketLambdaIntegration('connect-handler', connectLambda),
+      },
+      disconnectRouteOptions: {
+        integration: new WebSocketLambdaIntegration('disconnect-handler', disconnectLambda),
+      },
+    });
+
+    new WebSocketStage(this, 'prod-stage', {
+      webSocketApi: apiGateway,
+      stageName: 'prod',
+      autoDeploy: true,
     });
   }
 }
