e6f41ea208e03106b2c05cc03a33460ffc6a4754
Bryce Colson <bryzcolson@gmail.com>
2023-02-05 00:41:21 -0500

    Added reusable constructs

--- lib/stacks/game-state-stack.ts
+++ lib/stacks/game-state-stack.ts
@@ -1,13 +1,9 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { WebSocketApi, WebSocketStage } from '@aws-cdk/aws-apigatewayv2-alpha';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-import { WebSocketLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { join } from 'path';
 import { getCRUDItemPolicy, getPutItemPolicy } from './iam-policies';
+import { LambdaBackedWebSocketApi } from '../constructs/lambda-backed-web-socket-api';
 
 export class GameStateStack extends Stack {
   private readonly connectionsTable: Table;
@@ -37,78 +33,35 @@ export class GameStateStack extends Stack {
       writeCapacity: 1,
     });
 
-    const connectLambda = this.addLambda(
-      'connect',
-      {
-        TABLE_NAME: this.connectionsTable.tableName,
-      },
-      [getPutItemPolicy(this.connectionsTable)]
-    );
+    const lambdaEnvironment = {
+      GAME_STATE_TABLE_NAME: this.gameStateTable.tableName,
+      CONNECTIONS_TABLE_NAME: this.connectionsTable.tableName,
+    };
 
-    const disconnectLambda = this.addLambda(
-      'disconnect',
-      {
-        CONNECTIONS_TABLE_NAME: this.connectionsTable.tableName,
-        GAME_STATE_TABLE_NAME: this.gameStateTable.tableName,
-      },
-      [getCRUDItemPolicy(this.connectionsTable), getCRUDItemPolicy(this.gameStateTable)]
-    );
-
-    const apiGateway = new WebSocketApi(this, 'game-state-api', {
-      connectRouteOptions: {
-        integration: new WebSocketLambdaIntegration('connect-handler', connectLambda),
-      },
-      disconnectRouteOptions: {
-        integration: new WebSocketLambdaIntegration('disconnect-handler', disconnectLambda),
-      },
-    });
-
-    this.addLambdaBackedRoute(
-      apiGateway,
-      'new-game',
-      {
-        GAME_STATE_TABLE_NAME: this.gameStateTable.tableName,
-        CONNECTIONS_TABLE_NAME: this.connectionsTable.tableName,
-      },
-      [getPutItemPolicy(this.gameStateTable), getCRUDItemPolicy(this.connectionsTable)]
-    );
-
-    this.addLambdaBackedRoute(
-      apiGateway,
-      'game-state',
-      {
-        GAME_STATE_TABLE_NAME: this.gameStateTable.tableName,
-        CONNECTIONS_TABLE_NAME: this.connectionsTable.tableName,
-      },
-      [getCRUDItemPolicy(this.gameStateTable), getCRUDItemPolicy(this.connectionsTable)]
-    );
-
-    new WebSocketStage(this, 'prod-stage', {
-      webSocketApi: apiGateway,
-      stageName: 'prod',
-      autoDeploy: true,
-    });
-  }
-
-  private addLambda(routeKey: string, environment: any, policies: PolicyStatement[]) {
-    const lambda = new NodejsFunction(this, `${routeKey}-function`, {
-      entry: join(__dirname, `../handlers/${routeKey}-handler.ts`),
+    const builtInProps = {
       runtime: Runtime.NODEJS_18_X,
-      environment,
+      environment: lambdaEnvironment,
+    };
+
+    const apiGateway = new LambdaBackedWebSocketApi(this, 'game-state-api', {
+      connectHandlerProps: { ...builtInProps, handlerAlias: 'connect' },
+      disconnectHandlerProps: { ...builtInProps, handlerAlias: 'disconnect' },
+      handlerBasePath: '../handlers',
     });
 
-    policies.forEach((policy: PolicyStatement) => {
-      lambda.addToRolePolicy(policy);
-    });
+    apiGateway.connectHandler!.addToRolePolicy(getPutItemPolicy(this.connectionsTable));
 
-    return lambda;
-  }
+    apiGateway.disconnectHandler!.addToRolePolicy(getCRUDItemPolicy(this.connectionsTable));
+    apiGateway.disconnectHandler!.addToRolePolicy(getCRUDItemPolicy(this.gameStateTable));
 
-  private addLambdaBackedRoute(api: WebSocketApi, routeKey: string, environment: any, policies: PolicyStatement[]) {
-    const lambda = this.addLambda(routeKey, environment, policies);
+    const newGameRoute = apiGateway.addLambdaBackedRoute('new-game', Runtime.NODEJS_18_X, lambdaEnvironment);
+    newGameRoute.addPolicy(getCRUDItemPolicy(this.connectionsTable));
+    newGameRoute.addPolicy(getPutItemPolicy(this.gameStateTable));
 
-    api.addRoute(routeKey, {
-      integration: new WebSocketLambdaIntegration(`${routeKey}-handler`, lambda),
-    });
+    const joinGameRoute = apiGateway.addLambdaBackedRoute('join-game', Runtime.NODEJS_18_X, lambdaEnvironment);
+    joinGameRoute.addPolicy(getCRUDItemPolicy(this.connectionsTable));
+    joinGameRoute.addPolicy(getCRUDItemPolicy(this.gameStateTable));
+
+    apiGateway.addStage('prod');
   }
 }
